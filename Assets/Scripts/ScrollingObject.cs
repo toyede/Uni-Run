0aa3f5595d38d7316928d6e11ac1c175b14b40c9
toyede <toyede@naver.com>
2023-03-04 15:55:17 +0900

    .
    .

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -5,6 +5,7 @@ public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 이동 속도
 
     private void Update() {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
         // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
     }
 }
