fbad991eb26cefa7faac6c05f4b38510682b4f1f
toyede <toyede@naver.com>
2023-03-14 22:28:28 +0900

    ..

--- Assets/Scripts/BackgroundLoop.cs
+++ Assets/Scripts/BackgroundLoop.cs
@@ -5,15 +5,21 @@ public class BackgroundLoop : MonoBehaviour {
     private float width; // 배경의 가로 길이
 
     private void Awake() {
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width = backgroundCollider.size.x;
         // 가로 길이를 측정하는 처리
     }
 
     private void Update() {
+        if (transform.position.x <= -width){
+            Reposition();
+        }
         // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
     }
 
     // 위치를 리셋하는 메서드
     private void Reposition() {
-        
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2) transform.position + offset;
     }
 }
